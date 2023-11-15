89f764167e01812bbccd6b019379d3e548aa673a
Treglaov <egort,123@yandex.ru>
2023-11-15 11:34:02 +0500

    commit

--- db/config/db.py
+++ db/config/db.py
@@ -1,3 +1,13 @@
 import pymongo 
 
 conn = pymongo.MongoClient()
+db = conn['info']
+
+post = {
+    'id': 1,
+    'test': 'test123'
+}
+
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+db.list_collection_names()
