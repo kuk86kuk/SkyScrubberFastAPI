02bf7457ced7e13ab37a9a07fe6944e4ec2aa2ec
YuriMelnikov <melnikov_203@mail.ru>
2023-11-23 00:45:32 +0500

    Изменение processing_directory_royters.py

--- app/routers/processing_directory_royters.py
+++ app/routers/processing_directory_royters.py
@@ -10,49 +10,47 @@ router = APIRouter(prefix='/processing_directory', tags=['processing_directory']
 
 
 @router.post('/all/')
-async def send_for_processing_directory(FileProcessed: Path, data: Data): # FileProcessed: Path,  изменить!!!
+async def send_for_processing_directory(path_data: Path, data: Data):
     '''
-    GET Функция: Функция отправляет директорий на обработку нейросетью.
-    Args:
-        path_to_directory: Путь к директории, которую необходимо отправить на обработку.
-        file_processed: Имя файла, который будет использоваться для обработки данных в директории.
-         data: Данные или параметры, необходимые для процесса обработки.
-    Return: 
-        Возвращает результат; 200 OK
+    Функция POST: Отправляет директорию на обработку нейросетью.
+    Аргументы:
+        path_data: JSON-объект, содержащий атрибуты "path_to_directory" и "file_processed".
+        data: Данные или параметры, необходимые для процесса обработки.
+    Возвращает: 
+        Результат; 200 OK
     '''  
-    path = replace_underscore_with_slash(path_to_directory)
+    path = replace_underscore_with_slash(path_data.path_to_directory)
     print(path)
 
     if not checks_exists_directories(path):
-        return f'Путь не верен или директории не существует {path}  проверьте корректность вода'
+        return f'Путь неверен или директория не существует {path}. Пожалуйста, проверьте правильность ввода.'
+    
     directories, path = get_all_directories(path)
     
     arr_json = []
-    for dirictory in directories:
-        path_to_dir = path + dirictory
-        json = tasks(path_to_dir, data)
-        arr_json.append(json)
+    for directory in directories:
+        path_to_dir = path + directory
+        json_result = tasks(path_to_dir, data)
+        arr_json.append(json_result)
     
     DBM.do_insert_arr_in_db(arr_json)
     return 200
-    
-    
 
 @router.post('/')
-async def processing_file(path_to_name_file: Path, data: Data): # path_to_name_file: Path,  изменить!!!
+async def processing_file(file_data: Path, data: Data):
     '''
-    GET Функция: Функция отправляет файл на обработку нейросетью.
-    Args:
-        file_processed: Имя файла, который будет использоваться для обработки данных в директории.
+    Функция POST: Отправляет файл на обработку нейросетью.
+    Аргументы:
+        file_data: JSON-объект, содержащий атрибуты "path_to_directory" и "file_processed".
         data: Данные или параметры, необходимые для процесса обработки.
-    Return: 
-        Возвращает результат; 200 OK
+    Возвращает: 
+        Результат; 200 OK
     '''
-    path = replace_underscore_with_slash(path_to_name_file)
+    path = replace_underscore_with_slash(file_data.path_to_directory)
 
     if not checks_exists_directories(path):
-        return {f'Путь не верен или директории не существует {path}  проверьте корректность вода'}
+        return f'Путь неверен или директория не существует {path}. Пожалуйста, проверьте правильность ввода.'
     
-    json = tasks(path, data)
-    DBM.do_insert_in_db(json)
+    json_result = tasks(path, data)
+    DBM.do_insert_in_db(json_result)
     return 200
