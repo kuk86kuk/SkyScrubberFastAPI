ec04a5f2cb5750c5254103268255d8ae1e8d85fd
melnikov <melnikov_203@mail.ru>
2023-12-01 02:29:05 +0500

    +endpoints tag_routers, task_routers

--- app/routers/tag_routers.py
+++ app/routers/tag_routers.py
@@ -27,6 +27,48 @@ async def create_tag(tag: Tag):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@router.get("/get_tag/{tag_id}")
+async def get_tag(tag_id: str):
+    try:
+        tag = await tags_collection.find_one({"_id": ObjectId(tag_id)})
+        if tag:
+            # Преобразование ObjectId в строку
+            tag["_id"] = str(tag["_id"])
+            return JSONResponse(content=tag, status_code=200)
+        else:
+            raise HTTPException(status_code=404, detail=f"Tag not found for id: {tag_id}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.delete("/delete_tag/{tag_id}")
+async def delete_tag(tag_id: str):
+    try:
+        result = await tags_collection.delete_one({"_id": ObjectId(tag_id)})
+        if result.deleted_count == 1:
+            return JSONResponse(content={"message": "Tag deleted successfully"}, status_code=200)
+        else:
+            raise HTTPException(status_code=404, detail="Tag not found")
+    except Exception as e:
+        error_message = f"Error in delete_tag: {e}"
+        print(error_message)
+        raise HTTPException(status_code=500, detail=error_message)
+
+@router.put("/update_tag_args/{tag_id}")
+async def update_tag_args(tag_id: str, new_args: dict):
+    try:
+        result = await tags_collection.update_one(
+            {"_id": ObjectId(tag_id)},
+            {"$set": {"args": new_args}}
+        )
+        if result.modified_count == 1:
+            return JSONResponse(content={"message": "Tag args updated successfully"}, status_code=200)
+        else:
+            raise HTTPException(status_code=404, detail="Tag not found")
+    except Exception as e:
+        error_message = f"Error in update_tag_args: {e}"
+        print(error_message)
+        raise HTTPException(status_code=500, detail=error_message)
+
 # async def get_task(task_id: str):
 #     task = await collection.find_one({"_id": ObjectId(task_id)})
 #     return task
