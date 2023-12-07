525fa891d0b84e236594f5601be8ef6a4ebea2c9
melnikov <melnikov_203@mail.ru>
2023-12-07 20:07:20 +0500

    Добавление UUID, status.http. Скрытие _id при get - запросах, get - запросы  по UUID

--- app/routers/tag_routers.py
+++ app/routers/tag_routers.py
@@ -1,9 +1,10 @@
 import os
 
-from fastapi import FastAPI, HTTPException, APIRouter
+from fastapi import FastAPI, HTTPException, APIRouter, status
 from fastapi.responses import JSONResponse
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
+from uuid import uuid4
 from bson import ObjectId
 from db.settingsDB import SettingsDB
 from ..models.collections_model import Task, Tag, Log
@@ -18,57 +19,54 @@ logs_collection = settingsDB.COLLECTION_LOGS
 @router.post("/create_tag")
 async def create_tag(tag: Tag):
     try:
-        created_tag = await tags_collection.insert_one(tag.dict())
-        tag_id = str(created_tag.inserted_id)
 
-        tag_folder_path = f"neuro/{tag.neuro_id}/{tag_id}"
+        created_tag = await tags_collection.insert_one(tag.dict())
+        inserted_id = created_tag.inserted_id # тут был tag_id
+
+        tag_folder_path = f"neuro/{tag.neuro_id}/{inserted_id}"
         os.makedirs(tag_folder_path, exist_ok=True)
 
-        await create_log_entry(tag_id=tag_id, rel_path_to_project=f"neuro/{tag.neuro_id}/{tag_id}")
+        await create_log_entry(tag_id=tag_id, rel_path_to_project=f"neuro/{tag.neuro_id}/{inserted_id}")
 
-        return JSONResponse(content={}, status_code=200)
+        return JSONResponse(content={"message": "Tag created successfully"}, status_code=status.HTTP_200_OK)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        return JSONResponse(content={"message": f"Failed to create tag. Error: {str(e)}"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.get("/get_tag/{tag_id}")
 async def get_tag(tag_id: str):
     try:
-        tag = await tags_collection.find_one({"_id": ObjectId(tag_id)})
+        # Проекция для исключения поля _id
+        projection = {"_id": 0}
+        tag = await tags_collection.find_one({"tag_id": tag_id}, projection=projection)
+
         if tag:
-            # Преобразование ObjectId в строку
-            tag["_id"] = str(tag["_id"])
-            return JSONResponse(content=tag, status_code=200)
+            return JSONResponse(content=tag, status_code=status.HTTP_200_OK)
         else:
-            raise HTTPException(status_code=404, detail=f"Tag not found for id: {tag_id}")
+            return JSONResponse(content={"message": "Tag not found"}, status_code=status.HTTP_404_NOT_FOUND)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        return JSONResponse(content={"message": f"Error: {str(e)}"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.delete("/delete_tag/{tag_id}")
 async def delete_tag(tag_id: str):
     try:
-        result = await tags_collection.delete_one({"_id": ObjectId(tag_id)})
+        result = await tags_collection.delete_one({"tag_id": tag_id})
         if result.deleted_count == 1:
-            return JSONResponse(content={"message": "Tag deleted successfully"}, status_code=200)
+            return JSONResponse(content={"message": "Tag deleted successfully"}, status_code=status.HTTP_200_OK)
         else:
-            raise HTTPException(status_code=404, detail="Tag not found")
+            return JSONResponse(content={"message": "Tag not found"}, status_code=status.HTTP_404_NOT_FOUND)
     except Exception as e:
-        error_message = f"Error in delete_tag: {e}"
-        print(error_message)
-        raise HTTPException(status_code=500, detail=error_message)
+        return JSONResponse(content={"message": f"Error: {str(e)}"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.put("/update_tag_args/{tag_id}")
 async def update_tag_args(tag_id: str, new_args: dict):
     try:
         result = await tags_collection.update_one(
-            {"_id": ObjectId(tag_id)},
+            {"tag_id": tag_id},
             {"$set": {"args": new_args}}
         )
         if result.modified_count == 1:
-            return JSONResponse(content={"message": "Tag args updated successfully"}, status_code=200)
+            return JSONResponse(content={"message": "Tag args updated successfully"}, status_code=status.HTTP_200_OK)
         else:
-            raise HTTPException(status_code=404, detail="Tag not found")
+            return JSONResponse(content={"message": "Tag not found"}, status_code=status.HTTP_404_NOT_FOUND)
     except Exception as e:
-        error_message = f"Error in update_tag_args: {e}"
-        print(error_message)
-        raise HTTPException(status_code=500, detail=error_message)
-
+        return JSONResponse(content={"message": f"Error: {str(e)}"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- app/utils/log_utils.py
+++ app/utils/log_utils.py
@@ -17,13 +17,14 @@ class TaskStatus(Enum):
     FAILED = auto()
     PAUSED = auto()
 
-async def create_log_entry(tag_id: str, rel_path_to_project: str) -> Log:
+async def create_log_entry(task_id: str, tag_id: str, rel_path_to_project: str) -> Log:
     # Определите статус лога, например, "IN_PROGRESS"
     log_status = TaskStatus.IN_PROGRESS.name
 
     # Создаем объект Log с использованием перечисления TaskStatus
     log = Log(
-        id=tag_id,
+        id=task_id,
+        tag_id=tag_id,
         status=log_status,
         name_doc="",
         path_to_doc="",
