010173fd2b957dbc347ac6d70875e85f48da1fdc
ArtyomLyakhov4work <ArtyomLyakhov4work@hotmail.com>
2023-12-25 21:22:42 +0500

    celery and fixies

--- app/utils/neuro_utils.py
+++ app/utils/neuro_utils.py
@@ -1,35 +1,22 @@
-#from detection import MaskInpainting, BigImage
+from celery import Celery
+#import detection
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
+import concurrent.futures
+import time
 
+celery = Celery('tasks', broker='redis://localhost:6379')
 
-# async def run_neural_network(neuro_id, kwargs, tag_folder_path):
-#          dev_opt_remove(kwargs, tag_folder_path)
+executor = ThreadPoolExecutor(max_workers=2)
 
-async def dev_opt_remove(kwargs, tag_folder_path):
-    try:
-        image_dir = kwargs.get("image_dir", "../images")
-        image_format = kwargs.get("image_format", "*.tif")
-        save_dir = kwargs.get("save_dir", tag_folder_path)
-        device = kwargs.get("device", "cuda")
-        sigma = kwargs.get("sigma", 21)
-        patch_shape = kwargs.get("patch_shape", [512, 512])
-        step_patch = kwargs.get("step_patch", [512, 512])
+@celery.task
+def run_neural_network(neuro_id, kwargs, tag_folder_path):
+    #dev_opt_remove(kwargs, tag_folder_path)
+    time.sleep(5)
 
-        mask_inpainting =  None#MaskInpainting(device=device, sigma=sigma)
-        mask_inpainting.retouch_dir(
-            root_dir=image_dir,
-            file_format=image_format,
-            save_dir=save_dir,
-            patch_shape=patch_shape,
-            dx=step_patch[0],
-            dy=step_patch[1]
-        )
-        return {"status": "success", "message": "Neural network processing completed."}
-    except Exception as e:
-        return {"status": "error", "message": f"Error during neural network processing: {str(e)}"} 
-
-# async def dev_opt_remove(kwargs, tag_folder_path):
+# def dev_opt_remove(kwargs, tag_folder_path):
 #     try:
-#         image_dir = kwargs.get("image_dir", "../images")
+#         image_dir = kwargs.get("image_dir", "images")
 #         image_format = kwargs.get("image_format", "*.tif")
 #         save_dir = kwargs.get("save_dir", tag_folder_path)
 #         device = kwargs.get("device", "cuda")
@@ -37,7 +24,7 @@ async def dev_opt_remove(kwargs, tag_folder_path):
 #         patch_shape = kwargs.get("patch_shape", [512, 512])
 #         step_patch = kwargs.get("step_patch", [512, 512])
 
-#         mask_inpainting = MaskInpainting(device=device, sigma=sigma)
+#         mask_inpainting = detection.MaskInpainting(device=device, sigma=sigma)
 #         mask_inpainting.retouch_dir(
 #             root_dir=image_dir,
 #             file_format=image_format,
--- celery/celery_task.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-
-app = Celery('my_celery_tasks', broker='pyamqp://guest@localhost//', backend='rpc://')
-
-@app.task
-def my_async_task(arg1, arg2):
-    return arg1 + arg2
