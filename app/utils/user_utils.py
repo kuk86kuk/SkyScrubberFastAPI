53c2851eb31da759942093cbd73e0fe7920e776a
melnikov <melnikov_203@mail.ru>
2023-12-25 23:40:36 +0500

    обновление utils

--- app/utils/user_utils.py
+++ app/utils/user_utils.py
@@ -7,4 +7,5 @@ class User(BaseModel):
 users_db = {
     "user1": User(username="user1", password="password1"),
     "user2": User(username="user2", password="password2"),
+    "user3": User(username="user3", password="password3"),
 }
